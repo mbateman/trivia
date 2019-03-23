32d49ddb7e22108206be6aefe5c1e7ab9c89f167
john <devnull@localhost>
2019-03-23 11:27:22 +0000

    Add golden master

---
+++ python/master.py
@@ -0,0 +1,28 @@
+from trivia import Game
+
+from random import randrange, seed
+
+
+
+
+for counter in range(1, 1000):
+    seed(counter)
+
+    not_a_winner = False
+
+    game = Game()
+
+    game.add('Chet')
+    game.add('Pat')
+    game.add('Sue')
+
+    while True:
+        
+        game.roll(randrange(5) + 1)
+
+        if randrange(9) == 7:
+            not_a_winner = game.wrong_answer()
+        else:
+            not_a_winner = game.was_correctly_answered()
+        
+        if not not_a_winner: break
